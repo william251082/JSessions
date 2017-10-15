ac39043d23d33a1671ba884f070dd997e21aa938
williamdelrosario <williamdelrosario@yahoo.com>
2017-10-15 11:53:41 +0200

    Created the app linked the urls created the models created the views did the template tagging

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from app.models import User
+
+def index(request):
+    return render(request,'app/index.html')
+
+def users(request):
+
+    user_list = User.objects.order_by('first_name')
+    user_dict = {'users':user_list}
+    return render(request, 'app/users.html',context=user_dict)
